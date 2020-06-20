7b1b4046c314d74f684e2ef60e4b72145e78c53c
DanielAlfonso17 <daniel.alfonso-b@mail.escuelaing.edu.co>
2020-06-19 23:46:36 -0500

    Primer push del proyecto entidades, servicios

---
+++ src/test/java/edu/escuelaing/alfonso/proyecto/arsw/SpringBootBackendProyectoArswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.alfonso.proyecto.arsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendProyectoArswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
